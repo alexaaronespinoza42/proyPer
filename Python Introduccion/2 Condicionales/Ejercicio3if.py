c07a233fb8156bcb1bbd60e5bb8141b5001cacce
alexaaronespinoza42 <131840728+alexaaronespinoza42@users.noreply.github.com>
2024-08-18 15:00:14 -0500

    Delete Python Introduccion directory
    Eliminado

--- Python Introduccion/2 Condicionales/Ejercicio3if.py
+++
@@ -1,9 +0,0 @@
-#Ejercicio 3 if
-
-nombre1 = input('Escribe un nombre: ')
-nombre2 = input('Escribe otro nombre: ')
-
-if nombre1[0] == nombre2[0] or nombre1[-1] == nombre2[-1]:
-    print('Coincidencia')
-else:
-    print('No hay Coincidencia')
--- Python Introduccion/3 Bucles Listas/arrayListas.py
+++
@@ -1,9 +0,0 @@
-#Introduccion a listas
-array=["futbol","pc",18.6,18,[6,7,10,4],True,False]
-print(array[1])
-print(array[4])
-print(array[-1])
-print(array[0:2])   #El 2 significa la cantidad de datos mas no el numero de la posicion del dato
-print(array[0:3])
-print(array[1:3])  
-print(array[2:]) 
